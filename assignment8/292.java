a1fb56ea4d06737d22150941f5414be2eb8d020f
yongcl3r <lycseal>
2015-12-13 13:30:12 +0800

    292题的代码和截图

---
+++ assignment8/292.java
@@ -0,0 +1,14 @@
+
+public class NimGame
+{
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+    public boolean canWinNim(int n) {
+        return false;
+    }
+}
